518aea0f046368ecd702cb4bfb068a96ff6a3717
Настя <115420411+nastiaermilova@users.noreply.github.com>
2024-06-05 16:42:18 +0300

    ChangeRecipeScreen
    Add:
    - Alert Dialog for Delete Button
    - Add Top Pannel
    - Change Buttons
    - Add Dropdown Menu

--- architecture-templates/app/src/main/java/com/sfedu/recipebook/ui/recipe/RecipeScreen.kt
+++ architecture-templates/app/src/main/java/com/sfedu/recipebook/ui/recipe/RecipeScreen.kt
@@ -3,6 +3,7 @@ package com.sfedu.recipebook.ui.recipe
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -35,6 +36,28 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.em
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import kotlinx.coroutines.launch
+
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+import androidx.compose.material3.AlertDialog
+
 @Composable
 fun RecipeScreen(
     onNavigateToMain: () -> Unit,
@@ -47,107 +70,178 @@ fun RecipeScreen(
         mutableStateOf(viewableIngredients)
     }
     if(recipe != null) {
-        Column (
-            modifier = Modifier .fillMaxWidth()
-                                .background(Color(0xFFE0F2F1)),
-        ){
-            Row(
-                Modifier.fillMaxWidth()
-                        .background(Color.White)
-                        .padding(10.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Button(
-                    onClick = {
-                        onNavigateToMain() },
-                    modifier = Modifier.width(156.dp),
-                    shape = RoundedCornerShape(15.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE0F2F1), contentColor = Color.Black),
-                    border = BorderStroke(2.dp, Color(0xFF4DB6AC))
-                    ) {
-                    Text(text = "Return")
-                }
-                Button(
-                    onClick = {
-                        viewModel.deleteRecipe(recipe)
-                        onNavigateToMain() },
-                    modifier = Modifier.width(156.dp),
-                    shape = RoundedCornerShape(15.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE0F2F1), contentColor = Color.Black),
-                    border = BorderStroke(2.dp, Color(0xFF4DB6AC))
-                    ) {
-                    Text(text = "Delete")
-                }
-            }
+        Column{
+            Scaffold(
+                topBar = {
+                    @OptIn(ExperimentalMaterial3Api::class)
+                    TopAppBar(
+                        title= { },
+                        navigationIcon={
+                            IconButton(onClick = { onNavigateToMain() }
+                            ) {
+                                Icon(Icons.Filled.ArrowBack, contentDescription = "Назад")
+                            }
+                        },
+                        actions={
 
-            LazyColumn(modifier = Modifier.fillMaxSize(),
-                contentPadding = PaddingValues(0.dp)) {
-                item {
-                    Row(
-                        Modifier.fillMaxWidth()
-                            //.background(Color.White)
-                            .padding(10.dp, 10.dp, 10.dp, 10.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = recipe.name,
-                            modifier = Modifier.width(256.dp)
-                                .height(30.dp)
-                                //.padding(10.dp, 10.dp, 10.dp, 0.dp)
-                                .background(Color(0xFFB2DFDB)),
-                            style = androidx.compose.ui.text.TextStyle(textIndent = TextIndent(10.sp, 5.sp), fontSize = 26.sp,),
+                            Column{
+                                var expanded by remember { mutableStateOf(false) }
 
-                            )
-                        Spacer(modifier = Modifier.height(20.dp))
-                    }
+                                Box {
+                                    IconButton(
+                                        onClick = { expanded = true }
+                                    ) {
+                                        Icon(Icons.Filled.MoreVert, contentDescription = "Доп действия")
+                                    }
+                                    DropdownMenu(
+                                        expanded = expanded,
+                                        onDismissRequest = { expanded = false },
+                                        Modifier.background(Color.White)
 
+                                    ) {
+                                        TextButton(
+                                            {
+                                                // TODO make navigation to ChangeRecipeScreen and add this screen
+                                            }
+                                        ){
+                                            Text("Change Recipe", color = Color.Black)
+                                        }
 
+                                        val openDialog = remember { mutableStateOf(false) }
+                                        TextButton(
+                                            { openDialog.value = true },
 
-                }
-                items(ingredients.value) {
-                    Row(Modifier.fillMaxWidth()) {
-                        Text(
-                            text = ingredientTripleToString(it),
-                            modifier = Modifier.padding(10.dp, 0.dp, 0.dp, 0.dp),
+                                        ){
+                                            Text("Delete recipe", color = Color.Black)
+                                        }
+                                        if (openDialog.value) {
+                                            AlertDialog(
+                                                onDismissRequest = { openDialog.value = false},
+                                                title = { Text("Delete recipe?") },
+                                                confirmButton = {
+                                                    Button(
+                                                        onClick = { viewModel.deleteRecipe(recipe)
+                                                            onNavigateToMain(); openDialog.value = false },
+                                                        //openDialog.value = false ,
+                                                        modifier = Modifier.width(116.dp),
+                                                        shape = RoundedCornerShape(15.dp),
+                                                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE0F2F1), contentColor = Color.Black),
+                                                        border = BorderStroke(2.dp, Color(0xFF4DB6AC))
+                                                    ) {
+                                                        Text("Delete")
+                                                    }
+                                                },
+                                                dismissButton = {
+                                                    Button(
+                                                        onClick = { openDialog.value = false },
+                                                        modifier = Modifier.width(116.dp),
+                                                        shape = RoundedCornerShape(15.dp),
+                                                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE0F2F1), contentColor = Color.Black),
+                                                        border = BorderStroke(2.dp, Color(0xFF4DB6AC))
+                                                    ) {
+                                                        Text("Cancel", fontSize = 12.sp)
+                                                    }
+                                                },
+                                                containerColor = Color.White,
+                                                titleContentColor = Color.Black,
+                                            )
+                                        }
+                                    }
+                                }
+                            }
+                        },
+
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = Color.White,
+                            //titleContentColor = Color.Black,
+                            navigationIconContentColor = Color(0xFF4DB6AC),
+                            actionIconContentColor = Color(0xFF4DB6AC)
                         )
-                    }
-                    Spacer(modifier = Modifier.height(10.dp))
-                }
-
-                item {
-                    Row(
-                        Modifier.fillMaxWidth()
-                            .background(Color.White)
-                            .padding(10.dp),
-                        horizontalArrangement = Arrangement.SpaceBetween
+                    )
+                },
+            ){
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it)
+                        .background(Color(0xFFE0F2F1)),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 24.dp)
                     ) {
-                        Button(onClick = {
-                            resetViewableIngredients(ingredients = ingredients)
-                            onNavigateToCalc() },
+                        LazyColumn(modifier = Modifier.fillMaxSize(),
+                            contentPadding = PaddingValues(0.dp)) {
+                            item {
+                                Row(
+                                    Modifier.fillMaxWidth()
+                                        //.background(Color.White)
+                                        .padding(10.dp, 10.dp, 10.dp, 10.dp),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(
+                                        text = recipe.name,
+                                        modifier = Modifier.width(256.dp)
+                                            .height(30.dp)
+                                            //.padding(10.dp, 10.dp, 10.dp, 0.dp)
+                                            .background(Color(0xFFB2DFDB)),
+                                        style = androidx.compose.ui.text.TextStyle(textIndent = TextIndent(10.sp, 5.sp), fontSize = 26.sp,),
 
-                            modifier = Modifier.width(156.dp),
-                            shape = RoundedCornerShape(15.dp),
-                            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE0F2F1), contentColor = Color.Black),
-                            border = BorderStroke(2.dp, Color(0xFF4DB6AC)),
-                        ) {
-                            Text(text = "Recalculate ingredients")
-                        }
-                        Button(onClick = { resetViewableIngredients(ingredients = ingredients) },
+                                        )
+                                    Spacer(modifier = Modifier.height(20.dp))
+                                }
 
-                            modifier = Modifier.width(156.dp),
-                            shape = RoundedCornerShape(15.dp),
-                            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE0F2F1), contentColor = Color.Black),
-                            border = BorderStroke(2.dp, Color(0xFF4DB6AC)),
-                        ) {
-                            Text(text = "Reset ingredients")
+
+
+                            }
+                            items(ingredients.value) {
+                                Row(Modifier.fillMaxWidth()) {
+                                    Text(
+                                        text = ingredientTripleToString(it),
+                                        modifier = Modifier.padding(10.dp, 0.dp, 0.dp, 0.dp),
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(10.dp))
+                            }
+
+                            item {
+                                Row(
+                                    Modifier.fillMaxWidth()
+                                        .background(Color.White)
+                                        .padding(10.dp),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Button(onClick = {
+                                        resetViewableIngredients(ingredients = ingredients)
+                                        onNavigateToCalc() },
+
+                                        modifier = Modifier.width(156.dp),
+                                        shape = RoundedCornerShape(15.dp),
+                                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE0F2F1), contentColor = Color.Black),
+                                        border = BorderStroke(2.dp, Color(0xFF4DB6AC)),
+                                    ) {
+                                        Text(text = "Recalculate ingredients")
+                                    }
+                                    Button(onClick = { resetViewableIngredients(ingredients = ingredients) },
+
+                                        modifier = Modifier.width(156.dp),
+                                        shape = RoundedCornerShape(15.dp),
+                                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE0F2F1), contentColor = Color.Black),
+                                        border = BorderStroke(2.dp, Color(0xFF4DB6AC)),
+                                    ) {
+                                        Text(text = "Reset ingredients")
+                                    }
+                                }
+                                Spacer(modifier = Modifier.height(10.dp))
+
+                                Text(text = recipe.recipeSteps,
+                                    modifier = Modifier.padding(10.dp, 0.dp, 10.dp, 0.dp),
+                                )
+                                Spacer(modifier = Modifier.height(10.dp))
+                            }
                         }
                     }
-                    Spacer(modifier = Modifier.height(10.dp))
-
-                    Text(text = recipe.recipeSteps,
-                        modifier = Modifier.padding(10.dp, 0.dp, 10.dp, 0.dp),
-                        )
-                    Spacer(modifier = Modifier.height(10.dp))
                 }
             }
         }
